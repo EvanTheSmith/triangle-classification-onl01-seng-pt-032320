25fd1527c6ef146b3444729349008e75e82862d0
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-04-24 15:30:45 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if !(@side_a+@side_b > @side_c) || !(@side_a+@side_c > @side_b) || !(@side_b+@side_c > @side_a)
+      raise TriangleError
+    else
+      if (@side_a == @side_b) && (@side_b == @side_c)
+        return :equilateral
+      elsif (@side_a == @side_b) || (@side_b == @side_c) || (@side_a == @side_c)
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
